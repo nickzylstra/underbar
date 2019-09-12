467002373a74b964478635ce0de94d0cc335a183
nickzylstra <nick.zylstra@gmail.com>
2019-09-12 12:18:30 -0700

    Completes reduce and part one

--- src/underbar.js
+++ src/underbar.js
@@ -132,13 +132,11 @@
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
-    let mappedCollection;
+    let mappedCollection = [];
 
-<<<<<<< HEAD
-    // for each
-=======
-    // another comment
->>>>>>> master
+    _.each(collection, function(el) {
+      mappedCollection.push(iterator(el));
+    });
 
     return mappedCollection;
   };
@@ -149,7 +147,7 @@
    * as an example of this.
    */
 
-  // Takes an array of objects and returns and array of the values of
+  // Takes an array of objects and returns an array of the values of
   // a certain property in it. E.g. take an array of people and return
   // an array of just their ages
   _.pluck = function(collection, key) {
@@ -182,6 +180,18 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    let accUndefined = accumulator === undefined;
+
+    _.each(collection, function(item) { // if removing if statement from loop is desired, reimplement each with but adjusting start position
+      if (accUndefined) {
+        accumulator = item;
+        accUndefined = false;
+      } else {
+        accumulator = iterator(accumulator, item);
+      }
+    });
+
+    return accumulator;
   };
 
 
